f29fe6b1c78a5e9ee0206c7a01757c3de30d0d8c
Lathan Falls <lathanfalls04@gmail.com>
2024-07-24 21:15:28 -0500

    figuring out javascript error

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-// template_8jr35k6
+// template_w3we91r
 // service_7zr24kf
 // yrJCFJpnEuTeBXgjR
-
+/*
 function contact(event) {
     event.preventDefault();
     emailjs
         .sendForm(
             'service_7zr24kf',
-            'template_8jr35k6',
+            'template_w3we91r',
             event.target,
             'yrJCFJpnEuTeBXgjR'
-        ).then(() => {
-            console.log('it worked1')
-        })
-}
+    ).then(() => {
+        console.log('it worked 1')
+    })
+} */
+
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " .modal__overlay--visible";
+    setTimeout(() => {
+        loading.classList.remove(".modal__overlay--visible");
+        success.classList += " .modal__overlay--visible";
+        console.log('it worked 1')
+    }, 1000);

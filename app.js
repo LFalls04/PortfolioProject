39886410a376bcbd2ea65e353e3d912afffa10bb
Lathan Falls <lathanfalls04@gmail.com>
2024-07-26 02:32:35 -0500

    added some styling and java

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// template_w3we91r
+ // template_w3we91r
 // service_7zr24kf
 // yrJCFJpnEuTeBXgjR
-/*
+
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
     emailjs
         .sendForm(
             'service_7zr24kf',
@@ -11,15 +14,22 @@ function contact(event) {
             event.target,
             'yrJCFJpnEuTeBXgjR'
     ).then(() => {
-        console.log('it worked 1')
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at lathanfalls04@gmail.com"
+        );
     })
-} */
+}
 
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " .modal__overlay--visible";
-    setTimeout(() => {
-        loading.classList.remove(".modal__overlay--visible");
-        success.classList += " .modal__overlay--visible";
-        console.log('it worked 1')
-    }, 1000);
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+      return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = !isModalOpen
+    document.body.classList += " modal--open";
+}
